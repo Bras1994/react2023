d3bc390c472427191a2173f2148e187b83b3317b
Brenda Santos <santosagustina.94@gmail.com>
2023-06-05 00:06:57 -0300

    add useEffect

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,12 +1,42 @@
-
-import './App.css'
+import { useEffect, useState } from 'react'
+import  './App.css'
 
 function App() {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState ({x:0, y:0})
+
+  //Pointer Move
+
+  useEffect (()=>{
+    console.log('efecto', {enabled})
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => { // cleanup method
+      console.log('cleanup')
+      window.removeEventListener('pointermove', handleMove)
+    }
+
+  },[enabled])
 
   return (
-    <>
-      <h3>Proyecto 3</h3>
-    </>
+
+    <main>
+      <div className="bolita" style={{
+      transform:  `translate(${position.x}px , ${position.y}px)`}}>
+      </div>
+      <button onClick={()=> setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} : seguir puntero
+      </button>
+    </main>
+
   )
 }
 
