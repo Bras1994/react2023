51f39e67f39b20441c6f86a924b590d18d19518f
Brenda Santos <santosagustina.94@gmail.com>
2023-06-10 00:29:57 -0300

    Create folder hook - CustomHooks

--- projects/04-PruebaTecnica/src/App.jsx
+++ projects/04-PruebaTecnica/src/App.jsx
@@ -1,34 +1,31 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from 'react'
 import './app.css'
+import {getRandomFact} from './service/facts.js'
+import{useCatImage} from './hooks/useCatImage.js'
 
 const CAT_REFIX_IMAGE_URL='https://cataas.com'
 
+
+
 export function App (){
     const [fact, setFact] = useState()
-    const [imgUrl, setImgUrl] = useState()
+    const { imgUrl } = useCatImage ({fact })
 
-    useEffect(()=>{
-        fetch('https://catfact.ninja/fact')
-        .then (res => res.json())
-        .then(data => {
-            const {fact} = data
-            setFact(fact)
-            const firstWord = fact.split(' ', 3).join(' ')
-            console.log(firstWord)
-
-            fetch(`https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`)
-                .then (res => res.json())
-                .then(response => {
-                    const {url} = response
-                    setImgUrl(url)
-                })
-        })
+    // para recuperar la cira al cargar la pagina
+    useEffect(()=> {
+        getRandomFact().then(newFact => setFact(newFact))
     },[])
+
+
+    const handleClick = async()=>{
+        const newFact = await getRandomFact()
+        setFact(newFact)
+    }
     
     return(
         <main>
             <h1>App de Gatitos</h1>
-
+            <button onClick={handleClick}>Get new fact</button>
             {fact && <p>{fact}</p>} 
             {imgUrl && <img className="img_cat" src={`${CAT_REFIX_IMAGE_URL}${imgUrl}`} alt={`Image extrated using the fisrt word for ${fact}`} /> }
 
---
+++ projects/04-PruebaTecnica/src/hooks/useCatImage.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react"
+
+export function useCatImage ({fact}){
+    const [imgUrl, setImgUrl] = useState()
+
+    useEffect(()=>{
+        if (!fact) return
+
+        const firstWord = fact.split(' ', 3).join(' ')
+        console.log(firstWord)
+
+        fetch(`https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`)
+                .then (res => res.json())
+                .then(response => {
+                    const {url} = response
+                    setImgUrl(url)
+                })
+    },[fact])
+    return { imgUrl }
+}
