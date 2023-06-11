2dbe8902574116d18153e237b0711663ae243cf7
Brenda Santos <santosagustina.94@gmail.com>
2023-06-11 00:55:19 -0300

    Se recuperan datos del form.

--- projects/05-Buscador-Peliculas/src/App.jsx
+++ projects/05-Buscador-Peliculas/src/App.jsx
@@ -1,15 +1,23 @@
 import './App.css'
+import { useRef } from 'react'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
 
 function App() {
   const {movies : mappedMovies} = useMovies()
+  const inputRef = useRef()
+  const handleSubmit = (event)=>{
+    event.preventDefault()
+    const value = inputRef.current.value //esta creando un objecto que solo va a cambiar despues del current  - Recuperar el elemento del dom
+    console.log(value)
+  }
+
   return(
     <>
       <header className='buscador'>
         <h3>Buscador de peliculas</h3>
-        <form action="" className="form">
-          <input type="text" placeholder='Avengers,Matrix, Avatar...' />
+        <form name='query' onClick={handleSubmit} action="" className="form">
+          <input ref={inputRef} type="text" placeholder='Avengers,Matrix, Avatar...' />
           <button type='submit'> Buscar </button>
         </form>
       </header>
